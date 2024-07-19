e956492324769196a2fbacea33610834db762376
rayssasantana <rayssasantana.br@gmail.com>
2024-07-19 11:07:57 -0300

    Alterações na página NotFound

--- src/components/NotFoundComponent.jsx
+++ src/components/NotFoundComponent.jsx
@@ -1,12 +1,20 @@
 import "../styles/NotFoundComponent.css";
 import erro from "../assets/erro.png";
+import { Button } from "react-bootstrap";
 
 function NotFoundComponent() {
     return (
         <>
-            <img src={erro} alt="Erro-404" width={500}/>
-            <h1 className="h1-notfound">Erro 404 - Página não encontrada</h1>
-            <p>Desculpe, ocorreu um erro, a página solicitada não foi encontrada!</p>
+            <main className="caixa-nf">
+                <img src={erro} alt="Erro-404" width={500} />
+                <h1 className="h1-notfound">Erro 404 - Página não encontrada</h1>
+                <p>Desculpe, ocorreu um erro, a página solicitada não foi encontrada!</p>
+                <div className="botao-nf">
+                <Button className="mt-1 w-100 btn-custom" type="submit">
+                    Entrar
+                </Button>
+                </div>
+            </main>
         </>
     );
 }
