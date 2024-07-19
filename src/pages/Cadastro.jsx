ed78a3606397297eae803e009c45b5563ce261ef
T4RUM <wesley-murat@hotmail.com>
2024-07-19 10:37:45 -0300

    Adicionando rotas nas paginas

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -1,10 +1,78 @@
+import { Button } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import "../styles/Login.css";
+
 function Cadastro() {
+  
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const navigate = useNavigate();
+
+  function entrar(data) {
+    loginUsuario(data.email, data.senha).then(() => {
+      toast.success("Bem-vindo(a)!");
+      navigate("/tarefas");
+    }).catch(() => {
+      toast.error("Email e/ou senha incorreta!");
+    });
+  }
+
+  function handleEntrarGoogle() {
+    // lógica para login com Google
+  }
+
   return (
-    <>
-      <h1>Página de Cadastro</h1>
-    </>
+    <main className="container">
+      <form className="form-section" onSubmit={handleSubmit(entrar)}>
+        <h1>Cadastrar</h1>
+       
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            className="form-control"
+            {...register("email", { required: "O email é obrigatório" })}
+          />
+          {errors.email && (
+            <small className="invalid">{errors.email.message}</small>
+          )}
+        </div>
+        <div>
+          <label htmlFor="senha">Senha</label>
+          <input
+            type="password"
+            id="senha"
+            className="form-control"
+            {...register("senha", {
+              required: "A senha é obrigatória",
+              minLength: { value: 6, message: "Mínimo de 6 caracteres." },
+            })}
+          />
+          {errors.senha && (
+            <small className="invalid">{errors.senha.message}</small>
+          )}
+        </div>
+        <Button className="mt-1 w-100 btn-custom" type="submit">
+          Cadastrar
+        </Button>
+        <Button
+          onClick={handleEntrarGoogle}
+          variant="danger"
+          className="mt-1 w-100"
+          type="button"
+        >
+          Cadastrar com o Google
+        </Button>
+      </form>
+    </main>
   );
 }
 
 export default Cadastro;
-
---
+++ src/pages/ListaCompra.jsx
@@ -0,0 +1,9 @@
+function ListaCompra() {
+  return (
+    <>
+      <h1>Pagina de lista de compras</h1>
+    </>
+  );
+}
+
+export default ListaCompra;
