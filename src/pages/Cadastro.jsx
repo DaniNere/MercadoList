cbebef4ad01d8857e94758fbb02a92c9b1f8b558
DaniNere <danisilvanere@gmail.com>
2024-07-19 15:49:16 -0300

    :bug: Correção de erros no css de login e cadastro

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -2,7 +2,6 @@ import { Button } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
-import "../styles/Login.css";
 import "../styles/Cadastro.css";
 import { FaGoogle } from "react-icons/fa";
 import { cadastrarUsuario, entrarGoogle } from "../firebase/auth";
@@ -35,72 +34,69 @@ function Cadastro() {
   }
 
   return (
-    <>
-      <main className="container">
-        <form className="form-section" onSubmit={handleSubmit(cadastrar)}>
-          <h1>Cadastro</h1>
-          <hr />
-          <div>
-            <label htmlFor="nome">Nome</label>
-            <input
-              type="text"
-              id="nome"
-              className="form-control"
-              placeholder="Digite seu nome"
-              {...register("nome", { required: true, maxLength: 150 })}
-            />
-            {errors.nome && (
-              <small className="invalid">O nome é inválido!</small>
-            )}
-          </div>
-          <div>
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              id="email"
-              className="form-control"
-              placeholder="Digite seu e-mail"
-              {...register("email", { required: true })}
-            />
-            {errors.email && (
-              <small className="invalid">O email é inválido!</small>
-            )}
-          </div>
-          <div>
-            <label htmlFor="senha">Senha</label>
-            <input
-              type="password"
-              id="senha"
-              className="form-control"
-              placeholder="Digite sua senha"
-              {...register("senha", {
-                required: "A senha é obrigatória",
-                minLength: {
-                  value: 6,
-                  message: "A senha deve ter acima de 6 caracteres",
-                },
-              })}
-            />
-            {errors.senha && (
-              <small className="invalid">{errors.senha.message}</small>
-            )}
-          </div>
-          <div className="mt-4">
-            <Button className="mt-1 w-100 btn-custom" type="submit">
-              Cadastrar
-            </Button>
-            <Button
-              variant="danger"
-              className="mt-1 w-100"
-              type="button"
-              onClick={handleEntrarGoogle}
-            >
-              Entrar com Google
-            </Button>
-          </div>
-        </form>
-      </main>
-    </>
+    <main className="cadastro-container">
+      <form className="cadastro-form-section" onSubmit={handleSubmit(cadastrar)}>
+        <h1 className="cadastro-h1">Cadastro</h1>
+        <div>
+          <label htmlFor="nome" className="cadastro-label">Nome</label>
+          <input
+            type="text"
+            id="nome"
+            className="cadastro-input form-control"
+            placeholder="Digite seu nome"
+            {...register("nome", { required: true, maxLength: 150 })}
+          />
+          {errors.nome && (
+            <small className="cadastro-small invalid">O nome é inválido!</small>
+          )}
+        </div>
+        <div>
+          <label htmlFor="email" className="cadastro-label">Email</label>
+          <input
+            type="email"
+            id="email"
+            className="cadastro-input form-control"
+            placeholder="Digite seu e-mail"
+            {...register("email", { required: true })}
+          />
+          {errors.email && (
+            <small className="cadastro-small invalid">O email é inválido!</small>
+          )}
+        </div>
+        <div>
+          <label htmlFor="senha" className="cadastro-label">Senha</label>
+          <input
+            type="password"
+            id="senha"
+            className="cadastro-input form-control"
+            placeholder="Digite sua senha"
+            {...register("senha", {
+              required: "A senha é obrigatória",
+              minLength: {
+                value: 6,
+                message: "A senha deve ter acima de 6 caracteres",
+              },
+            })}
+          />
+          {errors.senha && (
+            <small className="cadastro-small invalid">{errors.senha.message}</small>
+          )}
+        </div>
+        <div className="mt-4">
+          <Button className="cadastro-button mt-1 w-100 btn-custom" type="submit">
+            Cadastrar
+          </Button>
+          <Button
+            variant="danger"
+            className="cadastro-button mt-1 w-100"
+            type="button"
+            onClick={handleEntrarGoogle}
+          >
+            Entrar com Google
+          </Button>
+        </div>
+      </form>
+    </main>
   );
 }
 
