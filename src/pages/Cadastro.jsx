7322a14d65c2a45849e4575ed0d6b2e156b1f3b8
T4RUM <wesley-murat@hotmail.com>
2024-07-19 10:55:33 -0300

    Alterando estilização da pagina de login e cadastro

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -3,9 +3,10 @@ import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import "../styles/Login.css";
+import "../styles/Cadastro.css";
+import { FaGoogle } from "react-icons/fa";
 
 function Cadastro() {
-  
   const {
     register,
     handleSubmit,
@@ -14,13 +15,15 @@ function Cadastro() {
 
   const navigate = useNavigate();
 
-  function entrar(data) {
-    loginUsuario(data.email, data.senha).then(() => {
-      toast.success("Bem-vindo(a)!");
-      navigate("/tarefas");
-    }).catch(() => {
-      toast.error("Email e/ou senha incorreta!");
-    });
+  function cadastrar(data) {
+    cadastrarUsuario(data.nome, data.email, data.senha)
+      .then(() => {
+        toast.success(`Bem vindo (a)! ${data.nome}`);
+        navigate("/tarefas");
+      })
+      .catch((error) => {
+        toast.error("Um erro aconteceu!" + error.code);
+      });
   }
 
   function handleEntrarGoogle() {
@@ -28,50 +31,72 @@ function Cadastro() {
   }
 
   return (
-    <main className="container">
-      <form className="form-section" onSubmit={handleSubmit(entrar)}>
-        <h1>Cadastrar</h1>
-       
-        <div>
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            className="form-control"
-            {...register("email", { required: "O email é obrigatório" })}
-          />
-          {errors.email && (
-            <small className="invalid">{errors.email.message}</small>
-          )}
-        </div>
-        <div>
-          <label htmlFor="senha">Senha</label>
-          <input
-            type="password"
-            id="senha"
-            className="form-control"
-            {...register("senha", {
-              required: "A senha é obrigatória",
-              minLength: { value: 6, message: "Mínimo de 6 caracteres." },
-            })}
-          />
-          {errors.senha && (
-            <small className="invalid">{errors.senha.message}</small>
-          )}
-        </div>
-        <Button className="mt-1 w-100 btn-custom" type="submit">
-          Cadastrar
-        </Button>
-        <Button
-          onClick={handleEntrarGoogle}
-          variant="danger"
-          className="mt-1 w-100"
-          type="button"
-        >
-          Cadastrar com o Google
-        </Button>
-      </form>
-    </main>
+    <>
+      <main className="container">
+        <form className="form-section" onSubmit={handleSubmit(cadastrar)}>
+          <h1>Cadastro</h1>
+          <hr />
+          <div>
+            <label htmlFor="nome">Nome</label>
+            <input
+              type="text"
+              id="nome"
+              className="form-control"
+              placeholder="Digite seu nome"
+              {...register("nome", { required: true, maxLength: 150 })}
+            />
+            {errors.nome && (
+              <small className="invalid">O nome é inválido!</small>
+            )}
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              className="form-control"
+              placeholder="Digite seu e-mail"
+              {...register("email", { required: true })}
+            />
+            {errors.email && (
+              <small className="invalid">O email é inválido!</small>
+            )}
+          </div>
+          <div>
+            <label htmlFor="senha">Senha</label>
+            <input
+              type="password"
+              id="senha"
+              className="form-control"
+              placeholder="Digite sua senha"
+              {...register("senha", {
+                required: "A senha é obrigatória",
+                minLength: {
+                  value: 6,
+                  message: "A senha deve ter acima de 6 caracteres",
+                },
+              })}
+            />
+            {errors.senha && (
+              <small className="invalid">{errors.senha.message}</small>
+            )}
+          </div>
+          <div className="mt-4">
+            <Button className="mt-1 w-100 btn-custom" type="submit">
+              Cadastrar
+            </Button>
+            <Button
+              variant="danger"
+              className="mt-1 w-100"
+              type="button"
+              onClick={handleEntrarGoogle}
+            >
+              Entrar com Google
+            </Button>
+          </div>
+        </form>
+      </main>
+    </>
   );
 }
 
