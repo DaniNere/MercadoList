8c138f5708dcf2d33e2811f8a744b75813625146
rayssasantana <rayssasantana.br@gmail.com>
2024-07-22 11:10:41 -0300

    Alterações de estilo no componente AddItem

--- src/components/AddItemComponent.jsx
+++ src/components/AddItemComponent.jsx
@@ -7,6 +7,7 @@ import { NumericFormat } from "react-number-format";
 import { addCompra } from "../firebase/itens";
 import "../styles/AddItemComponent.css";
 import { UsuarioContext } from "../contexts/UsuarioContext";
+import { Link } from "react-router-dom";
 
 function AddItemComponent() {
   const {
@@ -63,6 +64,7 @@ function AddItemComponent() {
     }
   }
 
+<<<<<<< Updated upstream
     return (
         <main className="container">
             <form className="form-section" onSubmit={handleSubmit(cadastrar)}>
@@ -144,6 +146,115 @@ function AddItemComponent() {
             </form>
         </main>
     );
+=======
+  return (
+    <main className="container add-item-container">
+      <form className="form-section" onSubmit={handleSubmit(salvarItem)}>
+        <h1 className="h1-add-item">Adicionar Item</h1>
+        <div>
+          <label htmlFor="nome">Nome</label>
+          <input
+            type="text"
+            id="nome"
+            className="form-control"
+            placeholder="Digite o nome do item"
+            {...register("nome", { required: true, maxLength: 100 })}
+          />
+          {errors.nome && <small className="invalid">O nome é inválido!</small>}
+        </div>
+        <div>
+          <label htmlFor="preco">Preço</label>
+          <NumericFormat
+            id="preco"
+            className="form-control"
+            placeholder="Digite o preço"
+            thousandSeparator=""
+            decimalSeparator=","
+            allowNegative={false}
+            onValueChange={handlePrecoChange}
+            value={preco}
+            {...register("preco", {
+              required: "O preço é obrigatório",
+              validate: (value) =>
+                (value !== "" && !isNaN(value)) || "O preço é inválido",
+            })}
+          />
+          {errors.preco && (
+            <small className="invalid">{errors.preco.message}</small>
+          )}
+        </div>
+        <div>
+          <label htmlFor="quantidade">Quantidade</label>
+          <section className="quantidade">
+            <Button
+              className="mt-1 w-20 btn-custom"
+              type="button"
+              onClick={handleDecremento}
+            >
+              -
+            </Button>
+            {numero}
+            <Button
+              className="mt-1 w-20 btn-custom"
+              type="button"
+              onClick={handleIncremento}
+            >
+              +
+            </Button>
+          </section>
+        </div>
+        <div>
+          <label htmlFor="precoTotal">Preço Total</label>
+          <input
+            type="text"
+            id="precoTotal"
+            className="form-control"
+            value={`R$ ${precoTotal.toFixed(2)}`}
+            readOnly
+          />
+        </div>
+        <div>
+          <label htmlFor="categoria">Categoria</label>
+          <select
+            id="categoria"
+            className="form-select"
+            {...register("categoria")}
+          >
+            <option value="">Selecionar categoria</option>
+            <option value="Alimentos">Alimentos</option>
+            <option value="Hortifruti">Hortifruti</option>
+            <option value="Bebidas">Bebidas</option>
+            <option value="Higiene">Higiene</option>
+            <option value="Limpeza">Limpeza</option>
+            <option value="Outros">Outros</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="descricao">Descrição</label>
+          <textarea
+            id="descricao"
+            className="form-control"
+            {...register("descricao", { maxLength: 150 })}
+            style={{ resize: "none" }}
+          ></textarea>
+          {errors.descricao && (
+            <small className="invalid">A descrição é inválida</small>
+          )}
+        </div>
+        <div className="botoes-add-item mt-4">
+          <Link to="/">
+            <Button className="cancelar mt-1 me-1 w-40" variant="danger" type="button">
+              Cancelar
+            </Button>   
+          </Link>     
+          <Button className="mt-1 w-40 ms-1 btn-custom" type="submit">
+            Adicionar
+          </Button> 
+        </div>
+      </form>
+    </main>
+  );
+>>>>>>> Stashed changes
 }
 
 export default AddItemComponent;
